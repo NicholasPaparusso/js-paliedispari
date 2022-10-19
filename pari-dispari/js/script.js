513bb22968ff3cbab1fcc2342d8ec1dc11d6aa02
NicholasPaparusso <nicopaparusso@gmail.com>
2022-10-19 21:16:38 +0200

    [end]

--- pari-dispari/js/script.js
+++ pari-dispari/js/script.js
@@ -5,3 +5,41 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
+
+
+const choice = prompt('pari o dispari');
+const userNum = parseInt(prompt('inserisci un numero da 1 a 5'));
+console.log("hai scelto: ", choice);
+console.log("il numero scelto è: ", userNum);
+
+
+function random(){
+let randomNum = Math.ceil(Math.random()*5);
+return randomNum;
+}
+
+var randomNum = random();
+console.log("il numero della cpu è: ", randomNum)
+
+
+function sum(n1,n2){
+  numSum = n1 + n2;
+  return numSum;
+}
+ var somma = sum(userNum,randomNum)
+
+console.log(sum("la somma dei due numeri è: ", somma))
+
+ function compare(){
+  if(somma % 2 === 0 && choice === "pari"){
+    console.log('la somma dei numeri è pari - hai vinto');
+  }else if(somma % 2 === 0 && choice === "dispari" ){
+    console.log('la somma dei numeri è pari - hai perso')
+  }else if ( !(somma % 2 === 0) && choice ==="dispari"){
+    console.log('la somma dei numeri è dispari - hai vinto')
+  }else{
+    console.log('la somma dei numeri è dispari - hai perso')
+  }
+ }
+
+ let oddEven = compare();
