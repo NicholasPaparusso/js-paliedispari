624cd5be774699d6683f6dccfae33b920b0a72b5
NicholasPaparusso <nicopaparusso@gmail.com>
2022-10-19 18:53:31 +0200

    [add] input.focus & input.value ="";

--- parola-palindroma/js/script.js
+++ parola-palindroma/js/script.js
@@ -5,31 +5,35 @@ Creare una funzione per capire se la parola inserita è palindroma
 
 
 //Dichiaro gli elementi del dom.
-
+let word;
+let input = document.getElementById('insert-word');
 const btn = document.querySelector('.btn-np');
 const output = document.querySelector('#output');
-
 // Invoco la Funzione tramite l'eventListener del bottone
- btn.addEventListener('click', myfunction)
-
-//creo la funzione per controllare se la parola è palindroma o meno
-
-function myfunction(wordToCheck){
-  let word = document.querySelector('#insert-word').value;
-  let char;
-  let array = [];
-  let invertedWord;
-  // Creo un for che cicla fino a che "i" è minore della lunghezza della parola data
-  for(let i = 0; i < word.length; i++){
-    char = word[i];
-    array.push(char);
+ btn.addEventListener('click', function(){
+  reverse(word);
+})
+// Creo la mia funzione
+ function reverse(wordToCheck) {
+  input.focus();
+  word = document.querySelector('#insert-word').value;
+  if(word.length < 3){
+  output.innerText = "La parola non soddisfa i requisiti minimi";
+  return ;
+}else{
+  let reversedWord = ''
+  for(let i = word.length - 1; i >= 0; i--) {
+  reversedWord += word[i]
+    }
+    if (reversedWord === word){
+      output.innerText = "La parola è palindroma";
+      word = "";
+      input.value ="";
+    }else{
+      output.innerText = "La parola non è palindroma";
+      word = "";
+      input.value ="";
+    }
+    return reversedWord;
   }
-  invertedWord = array.reverse(char);
-  console.log(invertedWord);
-  if(invertedWord = word){
-    
-  }
-  word ='';
- 
-
 }
